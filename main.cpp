9b51399055f7acb4bc02a41e01ff1f4bc0fa9232
eM dash <90864557+3mda5h@users.noreply.github.com>
2022-03-09 16:50:33 +0000

    made functions hehe

--- main.cpp
+++ main.cpp
@@ -9,6 +9,11 @@ struct Node
   char value[100];
 };
 
+int hashFunction(char* input, int size);
+void add(char* input, Node* hashTable[], int size);
+void print(char* input, Node* hashTable[], int size);
+void remove(char* input, Node* hashTable[], int size);
+
 int main() 
 {
   Node* hashTable[100] = {NULL}; //array of node pointers
@@ -25,82 +30,96 @@ int main()
     {
       cout <<"Enter name" << endl;
       cin.getline(input, 100);
-      //hash function
-      int hashValue = 0;
-      for(int i = 0; i < strlen(input); i++)
-      {
-        cout << "ascii: " << int(input[i]) << endl; 
-        hashValue += int(input[i]); //add ascii values lolol 
-      }
-      cout << "hash value is: " << hashValue << endl;
-      hashValue = hashValue % 100; //remainder of hashvalue divided by 100?
-      cout << "hash value is: " << hashValue << endl;
-      Node* newNode = new Node();
-      strcpy(newNode->value, input); 
-      if(hashTable[hashValue] == NULL)//if index is empty
-      {
-        hashTable[hashValue] = newNode;
-      }
-      else
-      {
-        Node* current = hashTable[hashValue];
-        while(current->next != NULL)
-        {
-          current = current->next;
-        }
-        current->next = newNode; //new node added to end of linked list
-      }
+      add(input, hashTable, sizeof(hashTable)/sizeof(hashTable[0]));
     }
     if(strcmp(input, "print") == 0) //print out all elements in hashtable
     {
-      for(int i = 0; i < sizeof(hashTable)/sizeof(hashTable[0]); i++)
-      {
-        if(hashTable[i] != NULL)
-        {
-          Node* current = hashTable[i];
-          while(current->next != NULL)
-          {
-            cout << current->value << ", ";
-            current = current->next;
-          }
-          cout << current->value << ", ";
-        }
-      }
-      cout << endl;
+      print(input, hashTable, sizeof(hashTable)/sizeof(hashTable[0]));
     }
     if(strcmp(input, "delete") == 0)
     {
       cout << "Name of person you want to delete?" << endl;
       cin.getline(input, 100);
-      int hashValue = 0;
-      for(int i = 0; i < strlen(input); i++)
+      remove(input, hashTable, sizeof(hashTable)/sizeof(hashTable[0]));
+    }
+  }
+}
+
+int hashFunction(char* input, int size)
+{
+  cout << "size: " << size << endl;
+  int hashValue = 0;
+  for(int i = 0; i < strlen(input); i++)
+  {
+    cout << "ascii: " << int(input[i]) << endl; 
+    hashValue += int(input[i]); //add ascii values lolol 
+  }
+  cout << "hash value is: " << hashValue << endl;
+  hashValue = hashValue % size; //remainder of hashvalue divided by size of hashtable
+  return hashValue;
+}
+
+void add(char* input, Node* hashTable[], int size)
+{
+  cout << "size in add: " << size << endl;
+  int hashValue = hashFunction(input, size);
+  cout << "hash value is: " << hashValue << endl;
+  Node* newNode = new Node();
+  strcpy(newNode->value, input); 
+  if(hashTable[hashValue] == NULL)//if index is empty
+  {
+    hashTable[hashValue] = newNode;
+  }
+  else
+  {
+    Node* current = hashTable[hashValue];
+    while(current->next != NULL)
+    {
+      current = current->next;
+    }
+    current->next = newNode; //new node added to end of linked list
+  }
+}
+
+void print(char* input, Node* hashTable[], int size)
+{
+  for(int i = 0; i < size; i++)
+  {
+    if(hashTable[i] != NULL)
+    {
+      Node* current = hashTable[i];
+      while(current->next != NULL)
       {
-        cout << "ascii: " << int(input[i]) << endl; 
-        hashValue += int(input[i]); //add ascii values lolol 
+        cout << current->value << ", ";
+        current = current->next;
       }
-      cout << "hash value is: " << hashValue << endl;
-      hashValue = hashValue % 100; //remainder of 100 divided by hashValue
-      cout << hashValue << endl;
-      Node* current = hashTable[hashValue];
-      if(current->next == NULL) //if first node is only node at this index
+      cout << current->value << ", ";
+    }
+  }
+  cout << endl;
+}
+
+void remove(char* input, Node* hashTable[], int size)
+{
+  int hashValue = hashFunction(input, size);
+  Node* current = hashTable[hashValue];
+  if(current->next == NULL) //if first node is only node at this index
+  {
+    cout << "1" << endl;
+    hashTable[hashValue] = NULL;
+    delete current;
+  }
+  else
+  {
+    while(current->next != NULL)
+    {
+      if(strcmp(current->value, input) == 0)
       {
-        cout << "1" << endl;
-        hashTable[hashValue] = NULL;
+        //current->previous->next = NULL;
         delete current;
+        break;
       }
-      else
-      {
-        while(current->next != NULL)
-        {
-          if(strcmp(current->value, input) == 0)
-          {
-            //current->previous->next = NULL;
-            delete current;
-            break;
-          }
-          current = current->next;
-        }
-      }
+      current = current->next;
     }
   }
 }
